52a4da6125d3846bdab4641da0205a956efc1498
lin <lin@HaodeMacBook-Pro-2.local>
2021-12-28 14:43:44 +0800

    debug for user.py line31 & 33

--- user.py
+++ user.py
@@ -27,10 +27,10 @@ def run():
     #only for test
     price_list = get_data("TSLA.csv")
     stock_curr = stock("TSLA", price_list)
-    print(price_list[1].date)
-    i = 0 
+    print(price_list[1].date)#price_list[1].date
+    i = 1#0 
     while i < len(price_list):
-        temp = price(price_list[i].symbol,price_list[i])
+        #temp = price(price_list[i].symbol,price_list[i])
         print(price_list[i].get_date())
         i += 1
     # date1 = date_generator('2021-01-02')
