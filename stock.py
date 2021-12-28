c328a064b2f024b72c98a25f799ecfd87175776e
Gilbert Wang <jwan3346@uni.sydney.edu.au>
2021-12-28 14:12:32 +0800

    改了生成datetime obejct的function，现在问题是如果用loop检索price list里的price，这些元素好像实例化不了

--- stock.py
+++ stock.py
@@ -1,11 +1,42 @@
+import datetime
+from typing import List
+from price import price
+
 class stock:
-    def __init__(self, symbol, price_list):
+    def __init__(self, symbol: str, price_list: List[price]):
         self.symbol = symbol
         self.price_list = price_list
-        pass
+
 
     def get_return(period):
         pass
 
     def get_beta(period):
         pass
+
+
+
+    def get_period_prices(self,start_date: datetime.date, end_date: datetime.date):
+        i = 0
+        target_list = [price]
+        while i < len(self.price_list):
+            p = self.price_list[i]
+            if (p.date > (start_date - datetime.timedelta(day = 1)) and p.date < (end_date + datetime.timedelta(day = 1))):
+                target_list.append(p)
+            i += 1
+        return target_list
+                
+        # if start_date.isoweekday == 6 :
+        #     new_start_date = start_date + datetime.timedelta(days=2)
+        # if start_date.isoweekday == 7 :
+        #     new_start_date = start_date + datetime.timedelta(days=1)
+        # if end_date.isoweekday == 6:
+        #     new_end_date = end_date - datetime.timedelta(days=1)
+        # if end_date.isoweekday == 7:
+        #     new_end_date = end_date - datetime.timedelta(days=2)
+        # new_start_date = start_date
+        # new_end_date = end_date
+
+        
+
+
--- user.py
+++ user.py
@@ -1,13 +1,14 @@
 import csv
-from price import Price
+from typing import List
+from stock import stock
+from price import price
 import datetime
 
-price_list = []#后面能直接用 原处在line9
 
 def get_data(filename):
         csvFile = open(filename, "r")
         reader = csv.reader(csvFile)
-        #price_list = []
+        price_list = [price]
         for info in reader:
             if reader.line_num == 1:
                 continue
@@ -18,25 +19,31 @@ def get_data(filename):
         return price_list
 
 def parse_price(symbol,curr_line):
-        date = curr_line[0]
-        newPrice = Price(symbol,date,float(curr_line[1]),float(curr_line[5]),int(curr_line[6]))
+        date = datetime.date.fromisoformat(curr_line[0])
+        newPrice = price(symbol,date,float(curr_line[1]),float(curr_line[5]),int(curr_line[6]))
         return newPrice
 
 def run():
     #only for test
-    get_data("TSLA.csv")
-    for p in price_list:
-        if p.date == '2020-12-21':
-            print("hit")
-        else:
-            print("false")
-            break
-    #
+    price_list = get_data("TSLA.csv")
+    stock_curr = stock("TSLA", price_list)
+    print(price_list[1].date)
+    i = 0 
+    while i < len(price_list):
+        temp = price(price_list[i].symbol,price_list[i])
+        print(price_list[i].get_date())
+        i += 1
+    # date1 = date_generator('2021-01-02')
+    # date2 = date_generator('2021-02-02')
+    # print(stock_curr.get_period_prices(date1, date2))
+    
+def date_generator(day: str):
+        return datetime.date.fromisoformat(day)
 
 if __name__ == '__main__':
     run()
 
-date = datetime('2020-12-21')
-print(date.year())
+
+
 
 
